59bb43c8b11544a1cd7232f173348a42e566e853
Raiduy <radu.apsan@gmail.com>
2024-07-20 23:29:46 +0200

    Checkpoint in implementation of code generator.

--- generator.py
+++ generator.py
@@ -5,6 +5,7 @@ import re
 import sys
 
 
+SOLUTIONS_FOLDER = './solutions/'
 EVOEVAL_DIFFICULT_IDS = ['61', '4', '79', '63', '90', '53', '66', '52', '16']
 
 
@@ -17,10 +18,47 @@ def get_dataset():
         df = pd.read_json("hf://datasets/evoeval/EvoEval_difficult/test.jsonl", lines=True)
         df.to_csv('.cache/difficult.csv', index=False)
     df = df[df['task_id'].isin(['EvoEval/'+i for i in EVOEVAL_DIFFICULT_IDS])]
-    print(['Evoeval/'+i for i in EVOEVAL_DIFFICULT_IDS])
     return df
 
-if __name__ == '__main__':
+
+def process_input_line(line):
+    input = line.split("for i in [")[-1]
+    input = input[:-2]
+    print(input)
+    return "hi"
+
+
+def get_code_inputs(id):
     df = get_dataset()
-    print(df)
+    test_code = df[df['task_id'] == f'EvoEval/{id}']['test'].values[0]
+    input_line = ''
+    for line in test_code.split('\n'):
+        if 'inputs' in line:
+            input_line = line
+            break
+    inputs = process_input_line(input_line)
+    return inputs
+
+
+def process_llms(solutions_folder, all_llms):
+    for id in EVOEVAL_DIFFICULT_IDS:
+        inputs = get_code_inputs(id)
+        for llm in all_llms:
+            path = f"{solutions_folder}{llm}/{llm}/EvoEval_difficult/EvoEval_{id}/0.py"
+            print(path)
+            #code = get_solution_code(llm, id)
+            break
+        break
+
+if __name__ == '__main__':
+    solutions_folder = sys.argv[1] # SOLUTIONS_FOLDER
+    output_folder = sys.argv[2] # OUTPUT_FOLDER
+    if len(sys.argv) < 4:
+        all_llms = os.listdir(solutions_folder)
+        print("All folders:", all_llms)
+    else:
+        all_llms = [sys.argv[3]]
+
+    process_llms(solutions_folder, all_llms)
+
     
