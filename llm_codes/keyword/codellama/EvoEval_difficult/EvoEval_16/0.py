94c2a85d27b195491b8528e3f32da55419b53ab8
Raiduy <radu.apsan@gmail.com>
2024-09-26 22:30:26 +0200

    Fix runner and remove useless folder

--- llm_codes/keyword/codellama/EvoEval_difficult/EvoEval_16/0.py
+++
@@ -1,9 +0,0 @@
-
-    if length > len(string):
-        return {}
-    result = {}
-    for i in range(len(string) - length + 1):
-        substring = string[i:i + length]
-        distinct_characters = set(substring)
-        result[substring] = len(distinct_characters)
-    return result
--- llm_codes/run_getter_and_test.sh
+++ llm_codes/run_getter_and_test.sh
@@ -8,12 +8,12 @@ else
     echo "Reprompting problem $3, from experiment $1, $2..."
     mkdir -p ./$1/$2/retries/
     python3 llm_code_getter.py $1 $2 $3 >> ./$1/$2/retries/$3.log
-    echo "" >> ./$1/$2/retries/$3.log
-    echo "---------------------------------------------------" >> ./$1/$2/retries/$3.log
-    echo "" >> ./$1/$2/retries/$3.log
     rm -f ./$1/$2/EvoEval_difficult/eval_results.json
     python3 ../../evoeval/evoeval/evaluate.py --dataset EvoEval_difficult --samples ./$1/$2/EvoEval_difficult/ | grep 'pass' >> ./$1/$2/retries/$3.log
     python3 -m json.tool ./$1/$2/EvoEval_difficult/eval_results.json &>> ./$1/$2/retries/$3.log
+    echo "" >> ./$1/$2/retries/$3.log
+    echo "---------------------------------------------------" >> ./$1/$2/retries/$3.log
+    echo "" >> ./$1/$2/retries/$3.log
 fi
 
 python3 ../../evoeval/evoeval/evaluate.py --dataset EvoEval_difficult --samples ./$1/$2/EvoEval_difficult/
