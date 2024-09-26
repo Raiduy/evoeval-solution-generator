8ad918a419d91a53325ad0607efbc2b6b2a658c5
Raiduy <radu.apsan@gmail.com>
2024-09-27 00:00:29 +0200

    Reprompt speechless-codellama for 52 and 61, fail

--- llm_codes/run_getter_and_test.sh
+++ llm_codes/run_getter_and_test.sh
@@ -8,7 +8,7 @@ if [ -z "$3" ]; then
     python3 -m json.tool ./$1/$2/EvoEval_difficult/eval_results.json &> ./$1/$2/EvoEval_difficult/eval_results_pretty.json
 
 else
-    echo "Reprompting problem $3, from experiment $1, $2..."
+    echo "Reprompting problem(s) $3, from experiment $1, $2..."
     mkdir -p ./$1/$2/retries/
     python3 llm_code_getter.py $1 $2 $3 >> ./$1/$2/retries/$3.log
     rm -f ./$1/$2/EvoEval_difficult/eval_results.json
